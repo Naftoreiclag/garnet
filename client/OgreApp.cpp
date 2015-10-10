14a730588a529e0838672389d3885f70563c375d
Naftoreiclag <naftoreiclag@gmail.com>
2015-10-10 10:26:11 -0700

    Add project files

---
+++ client/OgreApp.cpp
@@ -0,0 +1,16 @@
+#include "OgreApp.hpp"
+
+namespace grt
+{
+
+OgreApp::OgreApp()
+{
+}
+
+OgreApp::~OgreApp()
+{
+}
+
+
+}
+
---
+++ client/OgreApp.hpp
@@ -0,0 +1,17 @@
+#ifndef GRT_OGREAPP_HPP
+#define GRT_OGREAPP_HPP
+
+namespace grt
+{
+
+class OgreApp
+{
+public:
+    OgreApp();
+    ~OgreApp();
+
+};
+
+}
+
+#endif // GRT_OGREAPP_HPP
